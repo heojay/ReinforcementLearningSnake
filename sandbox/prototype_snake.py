932cefe3d866a6206d506cf2c85b0b6fb499dfc3
Adam Ross <14985050+R055A@users.noreply.github.com>
2019-04-20 22:10:11 +0200

    Add manual play option and snake growth message

--- sandbox/prototype_snake.py
+++ sandbox/prototype_snake.py
@@ -2,37 +2,37 @@
 
 from random import randint
 from time import sleep
-from sys import exit
+from sys import exit, argv
 from os import system
-
-N = 10  # the size of the snake game environment
 
 
 class Snake:
     """
     Prototype for the snake game
     @author: Adam Ross
-    @date: 18/04/2019
+    @date: 20/04/2019
     """
 
+    N = 10  # the size of the snake game environment
     TAIL_CHANCE = 5  # size of odds snake has chance of growing extra 'blocks'
     SLEEP_TIME = 2  # seconds that the game is paused between snake moves
 
-    def __init__(self, n=10):
+    def __init__(self, manual=False):
         """
         Initialize the Snake class
-        :param n: the size of the snake game environment
+        :param manual: Boolean for True if manual play, False if automated
         """
-        self.snake = [[randint(n // 3, n - n // 3), randint(n // 3, n - n // 3)]]
-        self.add_tail(n)
+        self.snake = [[randint(self.N // 3, self.N - self.N // 3),
+                       randint(self.N // 3, self.N - self.N // 3)]]
+        self.add_tail()
         self.direction = ""
-        self.go(n)
+        self.snake_grew = False
+        self.manual = manual
 
-    def move_snake(self, dir, n):
+    def move_snake(self, dir):
         """
         Moves snake one space forward, left or right of current head position
         :param dir: the direction the snake is moving; forward, left or right
-        :param n: the size of the snake game environment
         :return: True if the snake moves without crashing, False otherwise
         """
         if dir == 1:  # snake is continuing straight
@@ -47,17 +47,16 @@ class Snake:
             head = [self.snake[0][0] + (self.snake[0][1] - self.snake[1][1]),
                     self.snake[0][1] - (self.snake[0][0] - self.snake[1][0])]
             self.direction = "Snake turned right"
-        self.snake.pop()
 
-        if self.not_crashed(head, n):
+        if self.not_crashed(head):
+            self.snake.pop()
             self.snake[:0] = [head]
             return True
         return False
 
-    def add_tail(self, n):
+    def add_tail(self):
         """
         Adds an additional 'block' to the tail of the snake
-        :param n: the size of the snake game environment
         """
         if len(self.snake) == 1:
             if randint(0, 1):
@@ -70,50 +69,69 @@ class Snake:
                     tail = [self.snake[0][0] + 1, self.snake[0][1]]
                 else:
                     tail = [self.snake[0][0] + 1, self.snake[0][1]]
-            self.snake.append(tail)
-        tail = [self.snake[-1][0] + (self.snake[-1][0] - self.snake[-2][0]),
-                self.snake[-1][1] + (self.snake[-1][1] - self.snake[-2][1])]
+        else:
+            tail = [self.snake[-1][0] + (self.snake[-1][0] - self.snake[-2][0]),
+                    self.snake[-1][1] + (self.snake[-1][1] - self.snake[-2][1])]
 
-        if self.not_crashed(tail, n):
+        if self.not_crashed(tail):
             self.snake.append(tail)
+            self.snake_grew = True
 
-    def not_crashed(self, blc, n):
+    def not_crashed(self, b):
         """
         Determines if a snakes move/tail expansion does not result in a crash
-        :param blc: new 'block' being added to snake at either head or tail end
-        :param n: the size of the snake game environment
+        :param b: new 'block' being added to snake at either head or tail end
         :return: True if 'block' pos is available inside game, False otherwise
         """
-        return 0 <= blc[0] < n and 0 <= blc[1] < n and blc not in self.snake
+        return 0 <= b[0] < self.N and 0 <= b[1] < self.N and b not in self.snake
 
-    def display_snake(self, n):
+    def display_snake(self):
         """
         Displays the snake in the snake game environment
-        :param n: the size of the snake game environment
         """
         system('clear')
-        [print([' ' if [j, i] not in self.snake else '#' for i in range(n)])
-         for j in range(n)]
+        [print([' ' if [j, i] not in self.snake else '#' for i in
+                range(self.N)]) for j in range(self.N)]
+
+        if self.snake_grew:
+            print("Snake grew an extra block to its tail")
+            self.snake_grew = False
         print(self.direction)
 
-    def go(self, n):
+    def go(self):
         """
-        Recursively plays the snake game until the snake crashes
-        :param n: the size of the snake game environment
+        Plays the snake game manually/automated until the snake crashes
         """
-        self.display_snake(n)  # displays the snake and the board
+        self.display_snake()  # displays the snake and the board
 
         if not randint(0, self.TAIL_CHANCE):
-            self.add_tail(n)
-        sleep(self.SLEEP_TIME)  # pauses game between snake moves
+            self.add_tail()
 
-        if self.move_snake(randint(1, 3), n):  # if snake move without crashing
-            self.go(n)
+        if self.manual:
+            move = input("Enter 1 for forward, 2 for left, 3 for right:\n")
+
+            while move not in ['1', '2', '3']:
+                move = input("Enter 1 for forward, 2 for left, 3 for right:\n")
         else:
-            self.display_snake(n)  # displays the snake and the board
-            print("Snake crashed")
-            exit(1)
+            sleep(self.SLEEP_TIME)  # pauses game between snake moves
+            move = randint(1, 3)
+
+        if self.move_snake(int(move)):  # if snake move without crashing
+            self.go()
+
+    def stop(self):
+        """
+        Displays final game state, then exits program
+        """
+        self.display_snake()  # displays the snake and the board
+        print("Snake crashed")
+        exit(1)
 
 
 if __name__ == "__main__":
-    Snake(N)
+    if len(argv) > 1 and argv[1] == "-m":
+        app = Snake(True)
+    else:
+        app = Snake()
+    app.go()
+    app.stop()
