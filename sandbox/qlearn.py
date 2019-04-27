f4a81ea6f779033d9dc43d133d15fa6b43baa5b5
terratrends <johan.alfedeen@gmail.com>
2019-04-27 22:54:29 +0200

    work on qlearn with 4 matrixes

--- sandbox/qlearn.py
+++ sandbox/qlearn.py
@@ -2,29 +2,83 @@
 # Implementation of q-learning
 # 2019-04-22
 
+import numpy as np
+
 class qlearn:
-    learn_rate = 0.1 # alpha
-    discount = 0.95  # gamma
-    exploration = 1.0
-    default_reward = 0
+    lr = 0.1 # alpha learn_rate
+    disc = 0.95  # gamma discunt
+    expl = 1.0 # exploration
+    reward = 0 # default_reward
 
     def __init__(self, width, height):
         self.w = width
-        self.h = height
-        ###Matrix [[0 for x in range(self.w)] for y in range(self.h)] 
-        mtx = [[1, 2, 3, 4], [7, 8, 9]]
-        self.m = mtx
+        self.h = height        
+        self.mtx_north = np.random.rand(height, width)
+        self.mtx_south = np.random.rand(height, width)
+        self.mtx_east = np.random.rand(height, width)
+        self.mtx_west = np.random.rand(height, width)
 
-    def displayMatrix(self):    
-        print(self.m[0][0])
+    def getUpdateQValue(self, row, col, oldval):
+        cellmax = self.getMaxValue(row, col)
+        learnedval = (self.reward + self.disc * cellmax.maxval )
+        qv = (1-self.lr) * oldval + self.lr * learnedval
+        return qv
+
+    def getMaxValue(self, row, col):
+        v = self.mtx_north[row, col]
+        qc = qcell(row, col, v, "north")
+        if self.mtx_south[row, col] > v:
+            v = self.mtx_south[row, col]
+            qc = qcell(row, col, v, "south")
+        if self.mtx_east[row, col] > v:
+            v = self.mtx_east[row, col]
+            qc = qcell(row, col, v, "east")
+        if self.mtx_west[row, col] > v:
+            v = self.mtx_west[row, col]
+            qc = qcell(row, col, v, "west")
+        return qc
+
+    def getQValueNorth(self, row, col):
+        return self.mtx_north[row, col]
+
+    def getQValueSouth(self, row, col):
+        return self.mtx_south[row, col]
+
+    def getQValueEast(self, row, col):
+        return self.mtx_east[row, col]
+
+    def getQValueWest(self, row, col):
+        return self.mtx_west[row, col]
+
+    ##def displayMatrix(self, ):    
+        ###print(self.m[0][0])
+
+
+class qcell:
+    def __init__(self, row, col, maxval, maxdir):
+        self.row = row
+        self.col = col
+        self.maxval = maxval
+        self.maxdir = maxdir
 
 
 def main():
+    global lr, disc, expl, reward
+
     print("Begin qlearn")
     width = 10
     height = 10
     q = qlearn(width, height)
-    q.displayMatrix()
+    print(q.mtx_north)
+    v = q.getQValueEast(1,1)
+    print(v)
+    cellmax = q.getMaxValue(1,1)
+    print(cellmax.maxdir)
+    print(cellmax.maxval)
+
+    newqval = q.getUpdateQValue(1, 1, 0.5)
+    print(newqval)
+
     print("End qlearn")
 
 
