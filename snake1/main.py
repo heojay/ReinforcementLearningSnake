66e6ec7385e0449c75db9d86cb404919590cccdd
허재원 <yonseijaewon@gmail.com>
2019-05-01 17:52:19 +0200

    non-learning shortest path algorithm updated

--- snake1/main.py
+++ snake1/main.py
@@ -4,6 +4,14 @@
 
 import random
 import pygame
+import matplotlib.pyplot as plt
+'''
+import numpy as np
+from collections import deque
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+'''
 
 WIDTH = 500
 ROWS = 20
@@ -219,13 +227,17 @@ class Env:
         self.state_size = 4 #state
         self.action_size = 4
 
-    def reset(self):
-        self.s = snake((255, 0, 0), (10, 10))
-        self.snack = cube(randomSnack(ROWS, self.s), color=(0, 255, 0))
+    def get_state(self):
         head_pos = self.s.head.pos
         snack_pos = self.snack.pos
 
-        state = head_pos + snack_pos #Can be anything that you want.
+        return head_pos[0] - snack_pos[0], head_pos[1] - snack_pos[1]
+
+    def reset(self):
+        self.s = snake((255, 0, 0), (10, 10))
+        self.snack = cube(randomSnack(ROWS, self.s), color=(0, 255, 0))
+
+        state = self.get_state() #Can be anything that you want.
         return state, 0, False, 'start'
 
     def step(self, action):
@@ -241,7 +253,7 @@ class Env:
 
         else:
 
-            if head_pos[0] > ROWS or head_pos[0] < 0 or head_pos[1] > ROWS or head_pos[1] < 0:
+            if head_pos[0] >= ROWS or head_pos[0] < 0 or head_pos[1] >= ROWS or head_pos[1] < 0:
                 info = 'crash'
                 done = True
 
@@ -254,7 +266,7 @@ class Env:
         snack_pos = self.snack.pos
         reward = REWARD[info]
 
-        state = head_pos + snack_pos
+        state = self.get_state()
 
         return state, reward, done, info
 
@@ -271,14 +283,14 @@ def human():
     while flag:
         pygame.time.delay(DELAY)
         clock.tick(FPS)
-        s.move()
+        s.move_gym(shortest_pass(s, snack))
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(randomSnack(ROWS, s), color=(0, 255, 0))
 
         head = s.body[0].pos
 
-        if head[0] > ROWS or head[0] < 0 or head[1] > ROWS or head[1] < 0:
+        if head[0] >= ROWS or head[0] < 0 or head[1] >= ROWS or head[1] < 0:
             print("Score: ", len(s.body) - 1)
             flag = False
             break
@@ -294,24 +306,185 @@ def human():
     pass
 
 
+def shortest_pass(s, t):
+    head = s.head.pos
+    snack = t.pos
+
+    dirnx, dirny = s.dirnx, s.dirny
+    x_gap = snack[0] - head[0] #LEFT RIGHT
+    y_gap = snack[1] - head[1] #UP DOWN
+
+    if(x_gap == 0):
+        if(y_gap > 0):
+            return 3
+        else:
+            return 2
+
+    elif(y_gap == 0):
+        if(x_gap > 0):
+            return 1
+        else:
+            return 0
+
+    else:
+        if(dirnx * x_gap < 0):
+            if(y_gap > 0):
+                return 3
+            else:
+                return 2
+        else:
+            if(x_gap > 0):
+                return 1
+            else:
+                return 0
+
+
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+
+        # get size of state and action
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # These are hyper parameters for the DQN
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.999
+        self.epsilon_min = 0.01
+        self.batch_size = 64
+        self.train_start = 1000
+        # create replay memory using deque
+        self.memory = deque(maxlen=2000)
+
+        # create main model and target model
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+
+        # initialize target model
+        self.update_target_model()
+
+    # approximate Q function using Neural Network
+    # state is input and Q Value of each action is output of network
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(24, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(self.action_size, activation='linear',
+                        kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    # after some time interval update the target model to be same with model
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    # get action from model using epsilon-greedy policy
+    def get_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            q_value = self.model.predict(state)
+            return np.argmax(q_value[0])
+
+    # save sample <s,a,r,s'> to the replay memory
+    def append_sample(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    # pick samples randomly from replay memory (with batch_size)
+    def train_model(self):
+        if len(self.memory) < self.train_start:
+            return
+        batch_size = min(self.batch_size, len(self.memory))
+        mini_batch = random.sample(self.memory, batch_size)
+
+        update_input = np.zeros((batch_size, self.state_size))
+        update_target = np.zeros((batch_size, self.state_size))
+        action, reward, done = [], [], []
+
+        for i in range(self.batch_size):
+            update_input[i] = mini_batch[i][0]
+            action.append(mini_batch[i][1])
+            reward.append(mini_batch[i][2])
+            update_target[i] = mini_batch[i][3]
+            done.append(mini_batch[i][4])
+
+        target = self.model.predict(update_input)
+        target_val = self.target_model.predict(update_target)
+
+        for i in range(self.batch_size):
+            # Q Learning: get maximum Q value at s' from target model
+            if done[i]:
+                target[i][action[i]] = reward[i]
+            else:
+                target[i][action[i]] = reward[i] + self.discount_factor * (
+                    np.amax(target_val[i]))
+
+        # and do the model fit!
+        self.model.fit(update_input, target, batch_size=self.batch_size,
+                       epochs=1, verbose=0)
+
+
+
+
 env = Env()
-state_size = 1
+state_size = 2
 action_size = 4
 
-num_rounds = 1000
-num_samples = 100
+'''
+
+agent = DQNAgent(state_size, action_size)
+
+episodes = 1000
+
+scores = []
+
+for e in range(episodes):
+    done = False
+    s = 0
+    state, reward, done, info = env.reset()
+    state = np.reshape(state, [1,state_size])
+
+    while not done:
+
+        action = agent.get_action(state)
+        next_state, reward, done, info = env.step(action)
+        next_state = np.reshape(next_state, [1,state_size])
+        next_action = agent.get_action(next_state)
+        agent.append_sample(state, action, reward, next_state, done)
+
+        if len(agent.memory) >= agent.train_start:
+            agent.train_model()
+
+        s += reward
+        state = next_state
+
+        if done:
+            agent.update_target_model()
+            scores.append(s)
+    print(s)
+
+plt.plot(scores)
+plt.show()
 
 
 observation, reward, done, info = env.reset()
 
-for i in range(num_samples):
+for i in range(100):
+
     action = random.randint(0,3)
     print(observation, action, reward, info)
     next_observation, reward, done, info = env.step(action)
     observation = next_observation
     if done:
        observation, reward, done, info = env.reset()
+'''
 
-
-
-#I have trouble with env.reset(), will fix during weekend. Everything else seems fine.
+human()
